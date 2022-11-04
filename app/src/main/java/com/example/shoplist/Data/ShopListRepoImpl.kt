fd8f8aaf41f4da98b0513a961ff2953e589f225e
user <akimtom@yandex.ru>
2022-11-04 21:43:44 +0300

    Data Simple

---
+++ app/src/main/java/com/example/shoplist/Data/ShopListRepoImpl.kt
@@ -0,0 +1,35 @@
+package com.example.shoplist.Data
+
+import com.example.shoplist.Domain.ItemRepository
+import com.example.shoplist.Domain.ShoppingItem
+
+object ShopListRepoImpl:ItemRepository {
+
+    private var autoIncrementId  = 0
+    private val shopList= mutableListOf<ShoppingItem>()
+    override fun addShoppingItem(shoppingItem: ShoppingItem) {
+        if(shoppingItem.id == ShoppingItem.UNDEFINE_ID) {
+            shoppingItem.id = autoIncrementId++
+        }
+        shopList.add(shoppingItem)
+    }
+
+    override fun changeShoppingItem(shoppingItem: ShoppingItem) {
+        val oldShopItemElement  = getShoppingItem(shoppingItem.id)
+        shopList.remove(oldShopItemElement)
+        addShoppingItem(shoppingItem)
+    }
+
+    override fun deleteShoppingItem(shoppingItem: ShoppingItem) {
+        shopList.remove(shoppingItem)
+    }
+
+    override fun getShoppingItem(shoppingItemID: Int): ShoppingItem {
+        return shopList.find { it.id==shoppingItemID } ?:
+        throw RuntimeException("Id $shoppingItemID in shopping list not find  ASSHOLE")
+    }
+
+    override fun getShoppingList(): List<ShoppingItem> {
+        return shopList
+    }
+}
--- app/src/main/java/com/example/shoplist/Domain/ShoppingItem.kt
+++ app/src/main/java/com/example/shoplist/Domain/ShoppingItem.kt
@@ -1,4 +1,11 @@
 package com.example.shoplist.Domain
 
-data class ShoppingItem(var id:Int,val name: String, val count:Int,var isActive:Boolean) {
+data class ShoppingItem(
+    val name: String,
+    val count:Int,
+    var isActive:Boolean,
+    var id:Int = UNDEFINE_ID,) {
+    companion object{
+        const val UNDEFINE_ID = -1
+    }
 }
